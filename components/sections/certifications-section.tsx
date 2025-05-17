d9013983318975751761b0e81762f7e52e896afe
geekythe <geekyandbrain@gmail.com>
2025-05-17 22:52:23 +0300

    Update certifications-section.tsx

--- components/sections/certifications-section.tsx
+++ components/sections/certifications-section.tsx
@@ -220,7 +220,7 @@ export default function CertificationsSection() {
                             src={certification.image.url || "/placeholder.svg"}
                             alt={certification.title}
                             fill
-                            className="object-contain transition-transform duration-300 hover:scale-105"
+                            className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
                           />
                         </div>
                       </div>
