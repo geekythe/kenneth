e7a73da758f2fa396783c014e76f6013c16949e7
geekythe <geekyandbrain@gmail.com>
2025-05-18 09:38:28 +0300

    Update portfolio-section.tsx

--- components/sections/portfolio-section.tsx
+++ components/sections/portfolio-section.tsx
@@ -157,7 +157,7 @@ export default function PortfolioSection() {
                   <path d="M16.24 7.76a6 6 0 0 1 0 8.49m-8.48-.01a6 6 0 0 1 0-8.49m11.31-2.82a10 10 0 0 1 0 14.14m-14.14 0a10 10 0 0 1 0-14.14"></path>
                 </svg>
               </div>
-              <h2 className="text-5xl font-bold text-gray-700 mb-12">portfolio</h2>
+              <h2 className="text-5xl font-bold text-gray-700 mb-12">Portfolio</h2>
 
               {/* Filter Buttons */}
               <div className="flex flex-wrap justify-center gap-8 mb-12">
